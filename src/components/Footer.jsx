e37514559010fd5ef31d306fa7d92e31ff91a9b7
Mohamed Elyass <mohamedwathig3@gmail.com>
2026-02-26 21:32:46 +0200

    Update portfolio content and UI improvements12

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ export default function Footer() {
                 <p>© {new Date().getFullYear()} <a href="https://prebuiltui.com">PrebuiltUI</a>. All rights reserved.</p>
                 <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
                     <li><a target='_blank' href="https://github.com/Mohamedwathig">GitHub</a></li>
-                    <li><a target='_blank' href="www.linkedin.com/in/mohamed-elyass-238a98365">LinkedIn</a></li>
+                    <li><a target='_blank' href="https://www.linkedin.com/in/mohamed-elyass-238a98365/">LinkedIn</a></li>
                 </ul>
             </div>
         </div>
